8837f6d65cf719543cbf26ac77b7540c15988b87
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-25 16:33:33 -0400

    WebDesign Mobile Implemented

--- client/src/pages/GraphicDesign.jsx
+++ client/src/pages/GraphicDesign.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { ProjectLinks } from "../components";
+import { Header, Project } from "../components/UI";
+import BoxedWater from "../assets/graphic-design/desktop/image-boxed-water.jpg";
+import Change from "../assets/graphic-design/desktop/image-change.jpg";
+import Science from "../assets/graphic-design/desktop/image-science.jpg";
 
 const GraphicDesign = () => {
-  return <div>GraphicDesign</div>;
+  return (
+    <main>
+      <Header
+        title="Graphic Design"
+        desc="We deliver eye-catching branding materials that are tailored to meet your business objectives."
+      />
+      <section className="project-list">
+        <Project
+          img={Change}
+          title="Tim Brown"
+          desc="A book cover designed for 
+        Tim Brown’s new release, ‘Change’"
+        />
+        <Project
+          img={BoxedWater}
+          title="Boxed Water"
+          desc="A simple packaging concept made for Boxed Water"
+        />
+        <Project
+          img={Science}
+          title="Science!"
+          desc="A poster made in collaboration with the Federal Art Project"
+        />
+      </section>
+      <ProjectLinks isGraphic={true} />
+    </main>
+  );
 };
 
 export default GraphicDesign;
