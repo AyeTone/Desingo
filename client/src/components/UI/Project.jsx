c543ac3be875f344cd5529a21a73a5a3b81d8597
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-25 15:30:53 -0400

    Project implemented

--- client/src/components/UI/Project.jsx
+++ client/src/components/UI/Project.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({ title, desc, img }) => {
+  return (
+    <div className="project">
+      <figure className="project__img--wrapper">
+        <img src={img} alt="" />
+      </figure>
+      <div className="project__content">
+        <h3 className="project__content--title">{title}</h3>
+        <p className="project__content--decs">{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- client/src/pages/WebDesign.jsx
+++ client/src/pages/WebDesign.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Project } from "../components/UI";
 import Header from "../components/UI/Header";
+import Express from "../assets/web-design/desktop/image-express.jpg";
 
 const WebDesign = () => {
   return (
@@ -10,8 +11,12 @@ const WebDesign = () => {
         desc="We build websites that serve as powerful marketing tools
       and bring memorable brnad experiences."
       />
-      <section>
-        <Project />
+      <section className="project-list">
+        <Project
+          img={Express}
+          title="Express"
+          desc="A multi-carrier shipping website for ecommerce businesses"
+        />
       </section>
     </main>
   );
