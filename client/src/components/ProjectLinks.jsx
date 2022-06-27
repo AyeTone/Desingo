54ec1f3cde4818cf29730f2cdaf1269b786b9bb3
Troy Richards <94700828+AyeTone@users.noreply.github.com>
2022-06-27 12:23:57 -0400

    ProjectLinks Tablet Implemented

--- client/src/components/ProjectLinks.jsx
+++ client/src/components/ProjectLinks.jsx
@@ -1,57 +1,53 @@
 import React from "react";
-import Web from "../assets/home/mobile/image-web-design.jpg";
-import App from "../assets/home/mobile/image-app-design.jpg";
-import Graphic from "../assets/home/mobile/image-graphic-design.jpg";
 import { ReactComponent as RightArrow } from "../assets/shared/desktop/icon-right-arrow.svg";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ProjectLinks = ({ isWeb, isApp, isGraphic }) => {
+  const navigate = useNavigate();
+
   return (
     <section className="project-links">
       {!isWeb && (
-        <Link to="/webdesign" className="project-link">
-          <figure className="project-link__img--wrapper">
-            <img src={Web} alt="" />
-            <div className="project-link__content--bg">
-              <div className="project-link__content">
-                <h3>Web Design</h3>
-                <p>
-                  View Projects <RightArrow />{" "}
-                </p>
-              </div>
+        <figure onClick={() => navigate("/webdesign")} className="project-link">
+          <div className="project-link__img--web"></div>
+          <div className="project-link__content--bg">
+            <div className="project-link__content">
+              <h3>Web Design</h3>
+              <p>
+                View Projects <RightArrow />{" "}
+              </p>
             </div>
-          </figure>
-        </Link>
+          </div>
+        </figure>
       )}
       {!isApp && (
-        <Link to="/appdesign" className="project-link">
-          <figure className="project-link__img--wrapper">
-            <img src={App} alt="" />
-            <div className="project-link__content--bg">
-              <div className="project-link__content">
-                <h3>App Design</h3>
-                <p>
-                  View Projects <RightArrow />{" "}
-                </p>
-              </div>
+        <figure onClick={() => navigate("/appdesign")} className="project-link">
+          <div className="project-link__img--app"></div>
+          <div className="project-link__content--bg">
+            <div className="project-link__content">
+              <h3>App Design</h3>
+              <p>
+                View Projects <RightArrow />{" "}
+              </p>
             </div>
-          </figure>
-        </Link>
+          </div>
+        </figure>
       )}
       {!isGraphic && (
-        <Link to="/graphicdesign" className="project-link">
-          <figure className="project-link__img--wrapper">
-            <img src={Graphic} alt="" />
-            <div className="project-link__content--bg">
-              <div className="project-link__content">
-                <h3>Graphic Design</h3>
-                <p>
-                  View Projects <RightArrow />{" "}
-                </p>
-              </div>
+        <figure
+          onClick={() => navigate("/graphicdesign")}
+          className="project-link"
+        >
+          <div className="project-link__img--graphic"></div>
+          <div className="project-link__content--bg">
+            <div className="project-link__content">
+              <h3>Graphic Design</h3>
+              <p>
+                View Projects <RightArrow />{" "}
+              </p>
             </div>
-          </figure>
-        </Link>
+          </div>
+        </figure>
       )}
     </section>
   );
